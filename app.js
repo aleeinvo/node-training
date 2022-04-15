209e88dbdb2966e45c557e0f4cf5054b50f720a0
aleeinvo <ali.asghar@invozone.com>
2022-04-15 10:50:27 +0500

    create a basic node server

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const promise = Promise.reject('oops');
+const http = require('http');
+const fs = require('fs');
+const { stringify } = require('flatted');
+const util = require('util');
 
-promise.then(data => {
-    console.log(data);
-}).catch((reson) => {
-    console.log(reson);
-})
-.finally(() => {
-    console.log('We are done here.');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+
+    response.end(JSON.stringify({
+        message: 'Hello Dev!',
+        data: {
+            
+        }
+    }));
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}`);
 })
 
-console.log('still waiting');
+server.on('request', (request) => {
+    fs.writeFile('./node.log', util.inspect(request), {
+        flag: 'a+'
+    }, error => {
+        // console.log(error);
+    });
+})
